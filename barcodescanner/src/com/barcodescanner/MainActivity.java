e6e065867971cb8f4cef3fd3f599038b62f9d08d
felipe <felipe@giffic.com>
2013-01-29 03:24:45 -0300

    initial

---
+++ barcodescanner/src/com/barcodescanner/MainActivity.java
@@ -0,0 +1,39 @@
+package com.barcodescanner;
+
+import com.google.zxing.client.android.CaptureActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Intent intent = new Intent("com.barcode.SCAN");
+		intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+		startActivityForResult(intent, 0);
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == 0) {
+			if (resultCode == RESULT_OK) {
+				String contents = intent.getStringExtra("SCAN_RESULT");
+				String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+				Log.i("xZing", "contents: " + contents + " format: " + format);
+				// Handle successful scan
+			} else if (resultCode == RESULT_CANCELED) {
+				// Handle cancel
+				Log.i("xZing", "Cancelled");
+			}
+		}
+	}
+
+}
